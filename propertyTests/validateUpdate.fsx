11b41cd39f1edaa8c01e9f9e690d5399da65f4a4
Eric Smith <esmith@8thlight.com>
2017-02-08 08:12:13 -0600

    Begin writing a validator for the Update function
    This is mostly just a rename

---
+++ propertyTests/validateUpdate.fsx
@@ -0,0 +1,27 @@
+﻿
+#I @"../packages/"
+#load @"../src/fs/Constraints.fs"
+#load @"../src/fs/Game.fs"
+#r "FsCheck/lib/net45/FsCheck.dll"
+
+open SpaceInvaders.Game
+open FsCheck
+
+let invasion = {
+    SinceLastMove = 0.;
+    TimeToMove = 100.;
+    Direction = Invasion.Direction.Down;
+}
+
+let game = {
+    Entities = [];
+    LastUpdate = 0.;
+    Invasion = invasion;
+
+}
+
+type GameUpdateProperties = 
+  static member ``laser is never removed`` (game:Game) = 
+    true
+
+Check.QuickAll<GameUpdateProperties>()
