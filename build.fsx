5148b7e86a0307743707d1d74a75892d2e420ae2
Eric Smith <esmith@8thlight.com>
2017-02-26 10:33:21 -0600

    Add Upgrade/RemoveFableDep to build.fsx
    UpgradeFableDeps can upgrade specific dependency, and RemoveFableDep can
    remove them, via a `dep` flag.

--- build.fsx
+++ build.fsx
@@ -19,15 +19,26 @@ Target "Test" (fun _ -> Shell.Exec("yarn", "test") |> ignore)
 
 Target "Outdated" (fun _ -> Shell.Exec("yarn", "outdated") |> ignore)
 
-Target "UpgradeFableDeps" (fun _ -> Shell.Exec("yarn", "upgrade") |> ignore)
+Target "UpgradeFableDeps" (fun _ ->
+    let dep = getBuildParam "dep"
+    let upgradeCommand = sprintf "upgrade %s" dep
+
+    Shell.Exec("yarn", upgradeCommand) |> ignore)
+
+Target "RemoveFableDep" (fun _ ->
+    let dep = getBuildParam "dep"
+    match dep with
+    "" -> failwith "You must provide a dependency to remove"
+    _ ->
+      let removeCommand = sprintf "remove %s" dep
+      Shell.Exec("yarn", removeCommand) |> ignore)
 
 Target "TestWatch" (fun _ ->
     let testWatch = async { Shell.Exec("yarn", "test -- --watch") |> ignore }
 
     Async.Parallel [|testWatch|]
     |> Async.RunSynchronously
-    |> ignore
-)
+    |> ignore)
 
 Target "Watch" (fun _ ->
     let fableWatch = async { Shell.Exec ("yarn", "watch") |> ignore }
@@ -35,19 +46,16 @@ Target "Watch" (fun _ ->
     let testsWatch = async { Shell.Exec("yarn", "test -- --watch") |> ignore }
     Async.Parallel [|fableWatch; browser; testsWatch|]
     |> Async.RunSynchronously
-    |> ignore
-)
+    |> ignore)
 
 Target "BuildPropertyTest" (fun _ ->
     MSBuildDebug "./PropertyTest/build" "Build" ["./PropertyTest/PropertyTest.fsproj"]
-    |> Log "AppBuild-Output: "
-)
+    |> Log "AppBuild-Output: ")
 
 Target "PropertyTest" (fun _ ->
     let testDir = "./PropertyTest/build/"
     !! (testDir @@ "PropertyTests.dll")
-    |> xUnit2 (fun p -> {p with HtmlOutputPath = Some(testDir @@ "html")})
-)
+    |> xUnit2 (fun p -> {p with HtmlOutputPath = Some(testDir @@ "html")}))
 
 Target "All" DoNothing
 
