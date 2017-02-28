8e80b7e462c67c0a8eed742381a66d4ef602b199
Eric Smith <esmith@8thlight.com>
2017-02-28 08:01:49 -0600

    Fix the constraints
    The Width/Height are the cabinet constraints, whereas the left and right reflect
    the bounds of the gameplay area. It's possible this is off by a pixel or two.

--- SpaceInvaders/fs/Constraints.fs
+++ SpaceInvaders/fs/Constraints.fs
@@ -5,10 +5,10 @@ type Rect = {
     Right: int;
     Top: int
 }
-let Width = 217.
-let Height = 248.
+let Width = 224.
+let Height = 256.
 let Bounds = {
     Left = 3;
-    Right = 214;
+    Right = 220;
     Top = 15
 }
