8be5eb35149ee4a8bf7579bb187601ed808572a9
Eric Smith <esmith@8thlight.com>
2017-03-18 10:15:55 -0500

    Remove the laser can never be gone test

--- PropertyTest/Game.fs
+++ PropertyTest/Game.fs
@@ -19,23 +19,6 @@ let ``the number of invaders is always the same or lower after update`` (game : 
 
     updatedInvaderCount <= invaderCount
 
-type GameWithLaser =
-    static member GameWithLaser() =
-        Arb.Default.Derive()
-        |> Arb.filter (fun g -> Game.findLaser g.Entities <> None)
-
-type GameWithLaserPropertyAttribute() =
-    inherit PropertyAttribute(Arbitrary = [| typeof<GameWithLaser> |])
-
-
-[<GameWithLaserProperty>]
-let ``the laser is never removed`` (game : Game, delta : Delta) =
-    let newGame = Game.update game (Update delta)
-
-    let laser = Game.findLaser game.Entities
-
-    laser <> None
-
 let ``invaders never go beyond the left side`` () =
     ignore
 
